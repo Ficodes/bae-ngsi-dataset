948d0506359ba8ddb3aa22141299e11836c2e1b2
Francisco de la Vega <fdelavega@ficodes.com>
2019-05-21 15:05:06 +0200

    Activate CKAN datasets when the product is published

--- ngsi_dataset.py
+++ ngsi_dataset.py
@@ -20,7 +20,9 @@
 from __future__ import unicode_literals
 
 import requests
-from urlparse import urlparse
+from urlparse import urlparse, urljoin
+
+from django.conf import settings as django_settings
 
 from wstore.asset_manager.resource_plugins.plugin import Plugin
 from wstore.asset_manager.resource_plugins.plugin_error import PluginError
@@ -100,6 +102,12 @@ class NGSIDataset(Plugin):
             }]
         }
 
+        ckan_url = data_info['ckan_url']
+        if not ckan_url.endswith('/'):
+            ckan_url = ckan_url + '/'
+
+        ckan_url = ckan_url + 'api/3/action/package_create'
+
         resp = requests.post(ckan_url, json=dataset_info, headers={
             'Authorization': 'Bearer ' + self._get_access_token()
         })
@@ -108,6 +116,36 @@ class NGSIDataset(Plugin):
             raise PluginError('It had not being possible to create CKAN dataset')
 
         return resp.json()['result']
+
+    def update_dataset_acquire_url(self, url, dataset_id, product_id):
+        # Get updated dataset info
+        ckan_url = url
+        if not ckan_url.endswith('/'):
+            ckan_url = ckan_url + '/'
+
+        show_url = ckan_url + 'api/3/action/package_show?id={}'.format(dataset_id)
+        resp = requests.post(show_url, headers={
+            'Authorization': 'Bearer ' + self._get_access_token()
+        })
+
+        if resp.status_code != 200:
+            raise PluginError('It has not been posible to update acquire URL')
+
+        dataset_info = resp.json()['result']
+
+        # Generate Acquire URL
+        parsed_site = urlparse(django_settings.SITE)
+        acquire_url = '{}://{}/#/offering?productSpecId={}'.format(parsed_site.schema, parsed_site.netloc, product_id)
+
+        dataset_info['acquire_url'] = acquire_url
+
+        update_url = ckan_url + 'api/3/action/package_update?id={}'.format(dataset_id)
+        resp = requests.post(show_url, json=dataset_info, headers={
+            'Authorization': 'Bearer ' + self._get_access_token()
+        })
+
+        if resp.status_code != 200:
+            raise PluginError('It has not been posible to update acquire URL')
 
     def on_post_product_spec_validation(self, provider, asset):
         self._user_id = provider.name
@@ -128,7 +166,50 @@ class NGSIDataset(Plugin):
         asset.meta_info['app_id'] = app_id
         asset.save()
 
+    def update_product(self, product_spec, data_info):
+        product_url = django_settings.CATALOG
+        if not product_url.endswith('/'):
+            product_url = product_url + '/'
+        
+        product_url = product_url + 'api/catalogManagement/v2/productSpecification/{}'.format(product_spec['id'])
+        charact = product_spec['productSpecCharacteristic']
+
+        if 'entities' in data_info and data_info['entities'] is not None:
+            charact.append({
+                'configurable': False,
+                'description': 'NGSI Entities provided'
+                'name': 'Entities'
+                'productSpecCharacteristicValue': [{
+                    'value': data_info['entities'], 'unitOfMeasure': "", 'valueFrom': "", 'valueTo': "", 'default': True}]
+                'valueType': "string"
+            })
+
+        if 'attrs' in data_info and data_info['attrs'] is not None:
+            charact.append({
+                'configurable': False,
+                'description': 'NGSI Atributtes provided'
+                'name': 'Attributes'
+                'productSpecCharacteristicValue': [{
+                    'value': data_info['attrs'], 'unitOfMeasure': "", 'valueFrom': "", 'valueTo': "", 'default': True}]
+                'valueType': "string"
+            })
+
+        if 'expression' in data_info and data_info['expression'] is not None:
+            charact.append({
+                'configurable': False,
+                'description': 'NGSI Expression'
+                'name': 'Expression'
+                'productSpecCharacteristicValue': [{
+                    'value': data_info['expression'], 'unitOfMeasure': "", 'valueFrom': "", 'valueTo': "", 'default': True}]
+                'valueType': "string"
+            })
+
+        requests.patch(url, json={
+            'productSpecCharacteristic': charact
+        })
+
     def on_post_product_spec_attachment(self, asset, asset_t, product_spec):
+        self._user_id = asset.provider.name
         # Include NGSI specific info as characteristics of the product
         # If CKAN URL has been included register a new dataset
         if 'ckan_url' in asset.meta_info and asset.meta_info['ckan_url'] is not None and asset.meta_info['ckan_url'] != '':
@@ -139,6 +220,8 @@ class NGSIDataset(Plugin):
         #self.update_product(product_spec, asset.meta_info)
 
     def on_post_product_offering_validation(self, asset, product_offering):
+        self._user_id = asset.provider.name
+
         # Validate that the pay-per-use model (if any) is supported by the backend
         if 'productOfferingPrice' in product_offering:
             has_usage = False
@@ -153,13 +236,47 @@ class NGSIDataset(Plugin):
                                           price_model['unit'] + '. Supported units are: ' + ','.join(supported_units))
 
         # If CKAN dataset has been registered, attach acquisition URL
+        if 'dataset_id' in asset.meta_info:
+            self.update_dataset_acquire_url(data_info['ckan_url'], asset.meta_info['dataset_id'], asset.product_id)
+
+    def activate_ckan_dataset(self, ckan_url, dataset_id, customer):
+        notification_url = urljoin(ckan_url, '/api/action/package_acquired')
+        dataset_url = urljoin(ckan_url, '/dataset/{}'.format(dataset_id))
+
+        # Build notification data
+        data = {
+            'customer_name': customer,
+            'resources': [{
+                'url': dataset_url
+            }]
+        }
+
+        # Notify the dataset acquisition to CKAN
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(
+            notification_url,
+            json=data,
+            headers=headers,
+            verify=django_settings.VERIFY_REQUESTS,
+            cert=(django_settings.NOTIF_CERT_FILE, django_settings.NOTIF_CERT_KEY_FILE)
+        )
+        response.raise_for_status()
 
     def on_product_acquisition(self, asset, contract, order):
+        self._user_id = order.owner_organization.name
+
         # Activate API resources
         client = KeyrockClient()
         client.grant_permission(order.customer, asset.meta_info['role'])
 
+        if 'dataset_id' in asset.meta_info:
+            # User need to be included in the authorized users list of the dataset
+            self.activate_ckan_dataset(
+                asset.meta_info['ckan_url'], asset.meta_info['asset_id'], order.owner_organization.name)
+
     def on_product_suspension(self, asset, contract, order):
+        self._user_id = order.owner_organization.name
+
         # Suspend API Resources
         client = KeyrockClient()
         client.revoke_permission(order.customer, asset.meta_info['role'])
@@ -168,4 +285,5 @@ class NGSIDataset(Plugin):
         return self._units
 
     def get_pending_accounting(self, asset, contract, order):
-        pass
+        return []
+
